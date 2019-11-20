641a458b0c742fd27df63f461b7dcbc854e5dfd6
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-20 01:55:55 -0300

    Igor Gouveia aqui, terminei criação do tunelentre o App.js e o public/

--- stencil/src/App.js
+++ stencil/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  collapseTitle = 'Collapse Me!';
+
+  toggle() {
+      this.refs.collapsiblePanel.toggle();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <collapsible-panel ref="collapsiblePanel" title={this.collapseTitle}>
+              <ul>
+                <li>Components are awesome!</li>
+                <li>They drive the web</li>
+              </ul>
+          </collapsible-panel>
+          <button onClick={this.toggle.bind(this)}>Toggle from outside!</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
